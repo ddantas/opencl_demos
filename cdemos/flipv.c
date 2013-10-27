b3e286a63d6472f1369ec421b4d354c3964db70c
Luiz Santana <luizfelipedjs@hotmail.com>
2013-10-27 20:30:05 -0300

    --

--- cdemos/flipv.c
+++
@@ -1,8 +0,0 @@
-void flipv(IplImage *img){
-  int i, j;
-  unsigned char *data[];
-  for(i=0;i<img->width;i++)
-    for(j=0;j<img->height;j++)
-      data[i][j]=img->imageData[i][j];
-  for(i=0, j=1;i<(img->width*img->height);i++)
-    data[j][i]=img->imageData[i];
--- cdemos/libimage.h
+++
@@ -1,4 +0,0 @@
-//libimage.a
-void icolor(IplImage *img);
-IplImage* iread(char *fname);
-void iwrite(IplImage *img, char *fname);
