5f43dfa45f9768ab00eeb487567147ca0dcd0053
luizfelipedjs <luizfelipedjs@hotmail.com>
2014-06-22 13:59:29 -0300

    adding video.h to processing vglimage

---
+++ CLdemos/video.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cv.h>
+#include <highgui.h>
+#define MAX_DIM 3
+/*	Structure of 3D image */
+typedef struct _VglImage {
+	IplImage* ipl;
+	char* ndarray;
+	int ndim;
+	int shape[MAX_DIM];
+	int depth;
+	int nChannels;
+} VglImage;
+
+void copyIpl(IplImage*, IplImage*);
+void addNDarray(char*, VglImage*, int);
+VglImage* getVglImage(const char*);
